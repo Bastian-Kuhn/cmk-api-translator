7fa3f75cc0b14c18ea7ff2af185b8dfc83c6e0fd
Bastian Kuhn <mail@bastian-kuhn.de>
2023-02-28 16:50:42 +0100

    Added Support for Checkmk 2 RestAPI

--- application/api/cmk.py
+++ application/api/cmk.py
@@ -35,6 +35,28 @@ def verify_password(username, password):
         return check_password_hash(cfg_password, password)
     return False
 
+
+def get_job(data):
+    """
+    Determine by API Data what is to do
+    """
+    source_parts = data['ZIELID'].split('|')
+    job = False
+    service_name = False
+    if len(source_parts) == 2:
+        # Host Down
+        job = "host"
+    elif len(source_parts) == 3:
+        job = "service"
+        service_name = source_parts[2]
+    else:
+        raise ValueError("invalid id")
+
+    site = source_parts[0]
+    host_name = source_parts[1]
+
+    return job, site, host_name, service_name
+
 def create_payload(data):
     """
     Create a String with URL Payloads to set ACK
@@ -45,19 +67,19 @@ def create_payload(data):
         '_username' : app.config["CMK_USER"],
 
     }
-    source_parts = data['ZIELID'].split('|')
-    if len(source_parts) == 2:
-        # Host Down
+
+    job, site, host, svc = get_job(data)
+    if job == "host":
         payload['view_name'] = 'hoststatus'
-    elif len(source_parts) == 3:
-        payload['service'] = source_parts[2]
+    elif job == "service":
+        payload['service'] = svc
         payload['view_name'] = 'service_snow'
         #service down
     else:
         raise ValueError("invalid id")
 
-    payload['site'] = source_parts[0]
-    payload['host'] = source_parts[1]
+    payload['site'] = site
+    payload['host'] = host
 
     return "&".join([x+"="+urllib.parse.quote(y) for x, y in payload.items()])
 
@@ -110,6 +132,7 @@ class StatusAPI(Resource):
         except JSONDecodeError:
             return {"status": str(response.text)}
         except (ValueError, IndexError) as msg:
+            raise
             return {"status" :str(msg)}, 500
 
         return {"problem_solved" : solved}, 200
@@ -127,15 +150,32 @@ class AckApi(Resource):
         Set ACK on Host or Service
         """
         try:
-            payload_str = create_payload(request.json)
+            data = request.json
+            job, site, host, svc = get_job(data)
 
-            url = "{url}check_mk/view.py?_acknowledge=Acknowledge&_do_actions=yes"\
-            "&_do_confirm=yes&_transid=-1&{pl}".format(url=app.config['CMK_URL'],
-                                                       pl=payload_str)
-            # The thing is that cmk not has
-            # prober return status codes here,
-            # so we cannot make nothing...
-            requests.get(url, verify=app.config['SSL_VERIFY'])
+
+            cmk_url = app.config['CMK_URL']
+            payload = {
+              "sticky": False,
+              "persistent": False,
+              "notify": False,
+              "comment": f"Ticket : {data['QUELLEID']}",
+              "host_name": host
+            }
+            if job == "host":
+                url = f"{cmk_url}check_mk/api/1.0/domain-types/acknowledge/collections/host"
+                payload['acknowledge_type'] = "host"
+            elif job == "service":
+                url = f"{cmk_url}check_mk/api/1.0/domain-types/acknowledge/collections/service"
+                payload['acknowledge_type'] = "service"
+                payload['service_description'] = svc
+
+            username = app.config['CMK_USER']
+            password = app.config['CMK_SECRET']
+            headers = {
+                'Authorization': f"Bearer {username} {password}"
+            }
+            response = requests.post(url, json=payload, verify=app.config['SSL_VERIFY'], headers=headers)
         except (ValueError, IndexError) as msg:
             return {"status" :str(msg)}, 500
 
